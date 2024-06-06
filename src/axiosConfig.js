e0fc8e1dfb59e4ad5732a4acedc244db008df58f
Natan Simoes <natan16simoes@gmail.com>
2024-06-06 11:47:00 +0100

    update url

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://final-project-nsb7.onrender.com/", // Certifique-se de usar HTTP se seu servidor não estiver configurado para HTTPS
+  baseURL: "https://final-project-hfz7.onrender.com/", // Certifique-se de usar HTTP se seu servidor não estiver configurado para HTTPS
 });
 
 export default instance;
